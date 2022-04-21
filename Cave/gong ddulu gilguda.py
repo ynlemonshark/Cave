bfe1eb400db571ebcbab91fc79d513862d8b27c5
mudcook <mudcook@gmail.com>
2022-04-21 14:36:33 +0900

    Revert "- 가상환경을 새로 만들어 인터프리터도 변경함 - gym01.py로 안만들어서 다시 만들음 - 기본 q table 생성과 episode는 주석처리"
    This reverts commit b678a413

--- Cave/gong ddulu gilguda.py
+++ Cave/gong ddulu gilguda.py
@@ -4,23 +4,22 @@ import pygame
 import sys
 from pygame.locals import QUIT
 
-SIZE_X = 300
-SIZE_Y = 300
-
 pygame.init()
-SURFACE = pygame.display.set_mode((SIZE_X,SIZE_Y))
+SURFACE = pygame.display.set_mode((1000,1000))
 FPSCLOCK = pygame.time.Clock()
 
 gold = (255, 191, 0)
 
 
 def main():
-    s_x = 50
-    s_y = 50
-    wid_b = 20
-    hei_b = 20
-    FALL = True
-    vel = 10
+    xpos = 500
+    ypos = 0
+    gravity = 1
+    acceleration = 1
+    height = 800
+    mitigation = 10
+    rebound = 0
+    falling = True
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -29,37 +28,30 @@ def main():
 
             SURFACE.fill((255, 255, 0))
 
-            pygame.draw.ellipse(SURFACE, gold, (s_x, s_y, wid_b,hei_b))
-            if FALL:
-                s_y += vel
-                if (s_y + hei_b) >= SIZE_Y:
-                    FALL = False
+            pygame.draw.ellipse(SURFACE, gold, (xpos - 50,ypos - 50, 50, 50))
+
+            if falling:
+                acceleration += acceleration * gravity + gravity
+                height -= acceleration
+                if height <= 0:
+                    height = 0
+                    rebound = round(acceleration / mitigation, -1)
+                    falling = False
+                ypos = 1000 - height
+
             else:
-                s_y -= vel
-                if s_y <= 0:
-                    FALL = True
-            # if falling:
-            #     acceleration += acceleration * gravity + gravity
-            #     height -= acceleration
-            #     if height <= 0:
-            #         height = 0
-            #         rebound = round(acceleration / mitigation, -1)
-            #         falling = False
-            #     ypos = 1000 - height
-            #
-            # else:
-            #     acceleration -= rebound * gravity + gravity * 10
-            #     rebound -= round(rebound / 2, -1)
-            #     height += round(acceleration / 10, -1)
-            #     if rebound <= 1:
-            #         height = 0
-            #         falling = True
-            #     ypos = 1000 - height
-            #
-            # print(height)
+                acceleration -= rebound * gravity + gravity * 10
+                rebound -= round(rebound / 2, -1)
+                height += round(acceleration / 10, -1)
+                if rebound <= 1:
+                    height = 0
+                    falling = True
+                ypos = 1000 - height
+
+            print(height)
 
             pygame.display.update()
-            FPSCLOCK.tick(20)
+            FPSCLOCK.tick(50)
 
 if __name__ == '__main__':
     main()
